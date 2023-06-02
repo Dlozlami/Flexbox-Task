688cb3273d6508a7aabf9ce462dd98cb66a9a656
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-06-02 16:33:32 +0200

    Done

--- flexbox-task/src/App.js
+++ flexbox-task/src/App.js
@@ -5,19 +5,19 @@ function App() {
   return (
     <div className="App">
       <div className="FlexContainer" style={{height:'20vh'}}>
-        <Rectangle boxWidth={'33.33vw'} marginSpacing={0}  round={'0vw'}/>
-        <Rectangle boxWidth={'33.33vw'} marginSpacing={10}  round={'0vw'}/>
-        <Rectangle boxWidth={'33.33vw'} marginSpacing={0}  round={'0vw'}/>
+        <Rectangle boxWidth={'33.33vw'} marginSpacing={0}  round={false}/>
+        <Rectangle boxWidth={'33.33vw'} marginSpacing={10}  round={false}/>
+        <Rectangle boxWidth={'33.33vw'} marginSpacing={0}  round={false}/>
       </div>
 
       <div className="FlexContainer" style={{height:'60vh'}}>
-        <Rectangle boxWidth={'33.33vw'} marginSpacing={0}  round={'0vw'}/>
-        <Rectangle boxWidth={'33.33vw'} marginSpacing={10}  round={'100vw'}/>
-        <Rectangle boxWidth={'33.33vw'} marginSpacing={0}  round={'0vw'}/>
+        <Rectangle boxWidth={'33.33vw'} marginSpacing={0}  round={false}/>
+        <Rectangle boxWidth={'33.33vw'} marginSpacing={10}  round={true}/>
+        <Rectangle boxWidth={'33.33vw'} marginSpacing={0}  round={false}/>
       </div>
 
       <div className="FlexContainer" style={{height:'20vh'}}>
-        <Rectangle boxWidth={'100vw'} marginSpacing={0}  round={'0vw'}/>
+        <Rectangle boxWidth={'100vw'} marginSpacing={0}  round={false}/>
       </div>
     </div>
   );
--- flexbox-task/src/Components/Rectangle.js
+++ flexbox-task/src/Components/Rectangle.js
@@ -7,13 +7,13 @@ export default function Rectangle({boxWidth,marginSpacing,round}){
         backgroundColor:newColor,
         width:boxWidth,
         marginLeft:marginSpacing,
-        marginRight:marginSpacing,
-        borderRadius:round
+        marginRight:marginSpacing
     }
 
-    return(
-        <div className="Flexbox-rectangle" style={myStyles}>
+    if(round)
+    {
+        return(<div className="Circle" style={myStyles}></div>);
+    }
 
-        </div>
-    );
+    return(<div style={myStyles}></div>);
 }
