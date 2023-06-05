1480397769ee409befdf627b4a6d4017d8dda518
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-06-05 09:39:15 +0200

    Final Prod

--- flexbox-task/src/App.js
+++ flexbox-task/src/App.js
@@ -5,19 +5,19 @@ import Circle from './Components/Circle';
 function App() {
   return (
     <div className="App">
-      <div className="FlexContainer" style={{height:'20vh'}}>
+      <div className="TopandBottomDiv">
         <Rectangle boxWidth={'33.33vw'} marginSpacing={0} />
         <Rectangle boxWidth={'33.33vw'} marginSpacing={70} />
         <Rectangle boxWidth={'33.33vw'} marginSpacing={0} />
       </div>
 
-      <div className="FlexContainer" style={{height:'60vh',justifyContent:'space-between'}}>
+      <div className="CenterDiv">
         <Rectangle boxWidth={'20vw'} marginSpacing={0} />
         <Circle side={'20vw'} marginSpacing={0}/>
         <Rectangle boxWidth={'20vw'} marginSpacing={0} />
       </div>
 
-      <div className="FlexContainer" style={{height:'20vh'}}>
+      <div className="TopandBottomDiv">
         <Rectangle boxWidth={'100vw'} marginSpacing={0} />
       </div>
     </div>
